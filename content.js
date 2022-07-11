ee4860912060c9745c251cab5c8d03fc9dda9853
mechodev <mechakbiti33@gmail.com>
2022-07-11 17:47:18 +0100

    test2 extension

---
+++ content.js
@@ -0,0 +1,9 @@
+console.log("Hello", window.content);
+console.log(window.location);
+
+function selection() {
+  var content = window.content;
+  console.log(window.document.title);
+}
+document.write(window.location);
+/* document.addEventListener('click', selection) */
---
+++ extension.js
@@ -0,0 +1,15 @@
+ const list = document.getElementsByClassName("_1KDb8")
+const test = document.getElementById('app')
+   document.write( list.length) 
+ 
+document.write( window.getElementById('app').length) 
+
+/* $.get('https://web.whatsapp.com/', null, function(text){
+    /* alert($(text).find('#name')); 
+    document.write(window.document.toString())
+
+}); */
+
+
+document.write(window.document.toString())
+/* document.innerHTML(window.document) */
